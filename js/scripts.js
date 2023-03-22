cfe44dc622c6d01885a503a77c43525ffc32b9f3
Gareth Grindeland <gt.grindeland@gmail.com>
2023-03-22 13:44:45 -0700

    Add event handler function to direct user to the correct link based on their age.

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+  const form = document.querySelector("form");
+  form.onsubmit = function(event) {
+    event.preventDefault();
+
+    let pollInfo = document.getElementById("over-18");
+    pollInfo.setAttribute("class", "hidden");
+    let under18Message = document.getElementById("under-18");
+    under18Message.setAttribute("class", "hidden");
+
+    const age = parseInt(document.querySelector("input#age").value);
+
+    if (age >= 18) {
+      pollInfo.removeAttribute("class");
+    } else  {
+      under18Message.removeAttribute("class");
+    }
+  };
+};
